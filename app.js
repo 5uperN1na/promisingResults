088d4857120ed789d6fe3a79be116eb51586138c
Paula Suarez <pauladzuck@hotmail.com>
2019-03-18 05:43:00 -0500

    Adding updates to repo.

--- app.js
+++ app.js
@@ -9,10 +9,22 @@ slowMath.add(6, 2)
         return slowMath.divide(product, 4)
     }).then((quotient) => {
         console.log(`Quotient of 16 and 4 is ${quotient}`);
-    
-    
+        return slowMath.subtract(quotient, 3)
+    }).then((subtractResult) => {
+        console.log(`Subtraction result of 4 and 3 is ${subtractResult}`);
+        return slowMath.add(subtractResult, 98)
+    }).then((addNumber) => {
+        console.log(`Addition of 1 and 98 is ${addNumber}`);
+        return slowMath.remainder(addNumber, 2)
+    }).then((remainNumber) => {
+        console.log(`The remainder of 99 and 2 is ${remainNumber}`);
 
 
 
 
-})
+
+
+
+
+
+    })
